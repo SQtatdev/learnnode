89300e6a78dfd5e2c27332d9ce6a28d4ff451a9f
Vjatseslav Rovlin <slava.ryvlin@gmail.com>
2024-10-07 11:22:10 +0300

    make index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// import common from './common.js';
+import common from './common.js';
+common.hello();
+import es from './es.js';
+es.hello();
+
+
+// const common = require('./common.js');
 // common.hello();
-// import es from './es.js';
+// const es = require('./es.js').default;
 // es.hello();
 
-
-const common = require('./common.js');
-common.hello();
-const es = require('./es.js').default;
-es.hello();
