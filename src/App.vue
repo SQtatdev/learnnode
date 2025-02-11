f0e6a84cb46a7821125bf7c4ea73f3e0c0df0a50
sqtatdev <slava.ryvlin@gmail.com>
2025-02-11 13:24:16 +0200

    Last Update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
-<script setup> 
-
+<script setup>
+import Tabs from './comp/Tabs.vue';
 import { ref } from 'vue';
-import Modal from './comp/Modal.vue';
-let modalActive = ref(false);
-let modalActive2 = ref(false)
+
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+
+let titles = [
+    'ToDO',
+    'Modals',
+];
+
+let contents = [
+    ToDo,
+    Modals,
+]
+
+let activeTab = ref(0);
+function change(key){
+    activeTab.value = key;
+}
+
 </script>
-
 <template>
-    <div class="container mt-3">
-        <div class="buttons">
-            <button class="button is-primary" @click="modalActive=true">Open Modal</button>
-            <button class="button is-warning" @click="modalActive2=true">Open Modal2</button>
-        </div>
-        
-            <Modal :active="modalActive" @click="modalActive=false" url="https://picsum.photos/seed/sigmaboy/1280/960?random=1"> </Modal>
-            <Modal :active="modalActive2" @click="modalActive2=false"url="https://picsum.photos/1280/960?r=2"> </Modal>
+    <Tabs :items="titles" @change="change"></Tabs>
+    <component :is="contents[activeTab]"></component>
 
-    </div>
 </template>
-<style></style>
---
+++ src/pages/Modals.vue
@@ -0,0 +1,21 @@
+<script setup> 
+
+import { ref } from 'vue';
+import Modal from '../comp/Modal.vue';
+let modalActive = ref(false);
+let modalActive2 = ref(false)
+</script>
+
+<template>
+    <div class="container mt-3">
+        <div class="buttons">
+            <button class="button is-primary" @click="modalActive=true">Open Modal</button>
+            <button class="button is-warning" @click="modalActive2=true">Open Modal2</button>
+        </div>
+        
+            <Modal :active="modalActive" @click="modalActive=false" url="https://picsum.photos/seed/sigmaboy/1280/960?random=1"> </Modal>
+            <Modal :active="modalActive2" @click="modalActive2=false"url="https://picsum.photos/1280/960?r=2"> </Modal>
+
+    </div>
+</template>
+<style></style>
