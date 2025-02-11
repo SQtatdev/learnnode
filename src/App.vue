fa92690e6020bcbe3f1584b06020d9427cf97173
sqtatdev <slava.ryvlin@gmail.com>
2025-02-11 14:00:38 +0200

    new branch, router, rout, history saving

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <script setup>
-import Tabs from './comp/Tabs.vue';
+
 import { ref } from 'vue';
-
-import ToDo from './pages/ToDo.vue';
-import Modals from './pages/Modals.vue';
-
-let titles = [
-    'ToDO',
-    'Modals',
-];
-
-let contents = [
-    ToDo,
-    Modals,
-]
-
-let activeTab = ref(0);
-function change(key){
-    activeTab.value = key;
-}
+import RouterTabs from './comp/RouterTabs.vue';
+import { RouterView } from 'vue-router';
 
 </script>
 <template>
-    <Tabs :items="titles" @change="change"></Tabs>
-    <component :is="contents[activeTab]"></component>
+
+<RouterTabs>
+    
+</RouterTabs>
+
+<RouterView>
+
+</RouterView>
 
 </template>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import './style.scss';
 
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createWebHashHistory, createWebHistory, createMemoryHistory, createRouter } from 'vue-router';
 
-import App from './App.vue'
-const app = createApp(App)
+import App from './App.vue';
 
-app.mount('#app')
+import ToDo from './pages/ToDo.vue';
+import Modals from './pages/Modals.vue';
+
+const routes = [
+  { path: '/', component: ToDo, name: 'ToDo' },
+  { path: '/Modals', component: Modals, name: 'Modals' },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
