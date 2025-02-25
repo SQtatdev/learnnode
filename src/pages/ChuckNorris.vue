2eb612fbae60fda4c5c6572ebbfb88e85c4ac5d3
sqtatdev <slava.ryvlin@gmail.com>
2025-02-25 14:04:25 +0200

    asd

---
+++ src/pages/ChuckNorris.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+let joke = ref('');
+
+let response = await axios.get('https://api.chucknorris.io/jokes/random');
+joke.value = response.data.value;
+
+</script>
+
+<template>
+    {{ joke }}
+</template>
---
+++ src/pages/RickAndMorty.vue
@@ -0,0 +1,45 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import CharacterCard from '../comp/CharacterCard.vue';
+
+let characters = ref([]);
+let pagination = ref({});
+
+await getCharacters('https://rickandmortyapi.com/api/character');
+
+async function getCharacters(url) {
+    let response = await axios.get(url);
+    console.log(response.data);
+    characters.value = response.data.results;
+    pagination.value = response.data.info;
+}
+
+async function next(){
+    await getCharacters(pagination.value.next);
+}
+
+async function prev(){
+    await getCharacters(pagination.value.prev);
+}
+
+</script>
+
+<template>
+    <div class="buttons"> 
+        <button class="button is-primary" @click="prev" :disabled="!pagination.prev" >Previous</button>
+        <button class="button is-primary" @click="next" :disabled="!pagination.next">Next</button>
+    </div>
+    
+    <div class="columns is-multiline">
+        <div class="column is-one-quarter" v-for="character in characters">
+            <CharacterCard :character="character"> </CharacterCard>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    min-height: 100%;
+}
+</style>
